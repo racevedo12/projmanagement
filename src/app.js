157567fb3868347760a64588b06f709c59ae5bfd
racevedo12 <58488936+racevedo12@users.noreply.github.com>
2025-10-30 17:35:20 -0400

    Adding CORS and Setting up Middlewares and CORS configurations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Middlewares - Basic Configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// CORS Configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to basecampy!");
--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ dotenv.config({
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Listening app on port http://localhost:${port}`);
 });
