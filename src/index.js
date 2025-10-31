329bb997bc4fa8add5189e62c80ddaa51675f574
racevedo12 <58488936+racevedo12@users.noreply.github.com>
2025-10-31 19:12:16 -0400

    Adding Connection to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Listening app on port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App Listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error", err);
+    process.exit(1);
+  });
