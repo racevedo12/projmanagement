784d692bd860942b481fe91f6007ad6b90e4c350
racevedo12 <58488936+racevedo12@users.noreply.github.com>
2025-11-02 17:18:07 -0500

    Writing a healthcheck route and controller

--- src/app.js
+++ src/app.js
@@ -11,12 +11,17 @@ app.use(express.static("public"));
 // CORS Configurations
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+// Import The Routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to basecampy!");
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config({
   path: "./.env",
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
