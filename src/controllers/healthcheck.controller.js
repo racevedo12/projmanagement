3b7e272252522a7c460bb822c3e3cd9544795455
racevedo12 <58488936+racevedo12@users.noreply.github.com>
2025-11-02 17:26:20 -0500

    Adding async handler to make it easier and more readable

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,11 +1,10 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is up and running" }));
-  } catch (err) {}
-};
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { message: "Server is up and running" }));
+});
 
 export { healthCheck };
